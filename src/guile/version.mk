24523e99dde0f5c6c14b6176252352ad1154a357
Jim Hayes <jhayes@sdsc.edu>
2014-12-13 18:18:24 -0800

    Use local copy of bdwgc library, instead of expecting it to be installed.

--- src/guile/version.mk
+++ src/guile/version.mk
@@ -1,6 +1,6 @@
 NAME           = sdsc-guile
 VERSION        = 2.0.11
-RELEASE        = 1
+RELEASE        = 2
 PKGROOT        = /opt/guile
 
 SRC_SUBDIR     = guile
@@ -11,6 +11,12 @@ SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TAR_GZ_PKGS    = $(SOURCE_PKG)
+BDWGC_NAME     = bdwgc
+BDWGC_SUFFIX   = tar.gz
+BDWGC_VERSION  = 7.2alpha7
+BDWGC_PKG      = $(BDWGC_NAME)-$(BDWGC_VERSION).$(BDWGC_SUFFIX)
+BDWGC_DIR      = $(BDWGC_PKG:%.$(BDWGC_SUFFIX)=%)
+
+TAR_GZ_PKGS    = $(SOURCE_PKG) $(BDWGC_PKG)
 
 RPM.EXTRAS     = AutoReq:No
